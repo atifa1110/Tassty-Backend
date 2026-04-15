646ede3eba4735a58a091b946f18c0a0f6b0b2ef
atifafiorenza <atifafiorenza24@gmail.com>
2026-04-15 13:36:56 +0700

    fix: correct trust proxy

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ import { globalLimiter, authLimiter } from './middlewares/rateLimiter.js'
 dotenv.config()
 const app = express()
 
+app.set('trust proxy', 1);
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
